156151c97cc0f54696a69832e4717366a17ff297
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-05-10 19:44:07 +0430

    Create Home.jsx

---
+++ src/routes/Home.jsx
@@ -0,0 +1,46 @@
+import styles from '../styles/Home.module.css';
+
+const Home = () => (
+  <>
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <h2>Home</h2>
+      </div>
+      <div className={styles.content}>
+        <div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Dolores quo labore rerum
+            sunt debitis vitae odio mollitia neque,
+            accusamus rem optio saepe corporis ipsa
+            necessitatibus eaque,
+            culpa ratione quidem! Repellendus.
+            Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Dolores quo labore rerum
+            sunt debitis vitae odio mollitia neque,
+            accusamus rem optio saepe corporis ipsa
+            necessitatibus eaque,
+            culpa ratione quidem! Repellendus.
+          </p>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Dolores quo labore rerum
+            sunt debitis vitae odio mollitia neque,
+            accusamus rem optio saepe corporis ipsa
+            necessitatibus eaque,
+            culpa ratione quidem! Repellendus.
+            Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Dolores quo labore rerum
+            sunt debitis vitae odio mollitia neque,
+            accusamus rem optio saepe corporis ipsa
+            necessitatibus eaque,
+            culpa ratione quidem! Repellendus.
+          </p>
+        </div>
+      </div>
+    </div>
+  </>
+);
+export default Home;
