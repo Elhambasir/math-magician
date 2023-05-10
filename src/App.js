ec8e215cf10c2a7cb523a2c25f9e5184c85cd5cb
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-05-10 19:41:49 +0430

    Update App.js
    1. Import MyCulc component
    2. Implement React Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import MyCulc from './components/Calculator';
+import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import Layout from './components/Layout';
+import Home from './routes/Home';
+import NotMatch from './routes/NotMatch';
+import Calculator from './routes/Calculator';
+import Quote from './routes/Quote';
 
 function App() {
   return (
-    <>
-      <MyCulc />
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="Calculator" element={<Calculator />} />
+        <Route path="Quote" element={<Quote />} />
+      </Route>
+      <Route path="*" element={<NotMatch />} />
+    </Routes>
   );
 }
 
