b4ca305edd7f4b3427e4b07d3295555367713f0a
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-05-10 20:51:50 +0430

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles/app.css';
+import './styles/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
