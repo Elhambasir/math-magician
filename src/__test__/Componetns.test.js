8658a3d8bb371ca9ad2c5e1dfe2227b595b3f719
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-05-11 17:23:40 +0430

    Fix linter error

--- src/__test__/Componetns.test.js
+++ src/__test__/Componetns.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Home from '../routes/Home';
-import Quote from '../routes/quote';
+import Quote from '../routes/Quote';
 import Calculator from '../routes/Calculator';
 
 describe('Component render testing', () => {
